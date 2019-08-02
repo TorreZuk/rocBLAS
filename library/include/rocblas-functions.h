80810e130cddca7ec8351d43372a33501dfc00f7
First Last <torrezuk@cgy-pikachu.amd.com>
2019-08-02 16:43:40 -0600

    started complex additions

--- library/include/rocblas-functions.h
+++ library/include/rocblas-functions.h
@@ -350,10 +350,28 @@ ROCBLAS_EXPORT rocblas_status rocblas_dswap_batched(
     ********************************************************************/
 
 ROCBLAS_EXPORT rocblas_status rocblas_sswap_strided_batched(
-    rocblas_handle handle, rocblas_int n, float* x, rocblas_int incx, rocblas_int stridex, float* y, rocblas_int incy, rocblas_int stridey, rocblas_int batch_count);
+    rocblas_handle handle, rocblas_int n, float* x, rocblas_int incx, rocblas_int stridex, 
+    float* y, rocblas_int incy, rocblas_int stridey, rocblas_int batch_count);
 
 ROCBLAS_EXPORT rocblas_status rocblas_dswap_strided_batched(
-    rocblas_handle handle, rocblas_int n, double* x, rocblas_int incx, rocblas_int stridex, double* y, rocblas_int incy, rocblas_int stridey, rocblas_int batch_count);
+    rocblas_handle handle, rocblas_int n, double* x, rocblas_int incx, rocblas_int stridex,
+    double* y, rocblas_int incy, rocblas_int stridey, rocblas_int batch_count);
+
+ROCBLAS_EXPORT rocblas_status rocblas_cswap_strided_batched(rocblas_handle         handle,
+                                            rocblas_int            n,
+                                            rocblas_float_complex* x,
+                                            rocblas_int            incx, rocblas_int stridex,
+                                            rocblas_float_complex* y,
+                                            rocblas_int            incy, rocblas_int stridey, 
+                                            rocblas_int batch_count);
+
+ROCBLAS_EXPORT rocblas_status rocblas_zswap_strided_batched(rocblas_handle          handle,
+                                            rocblas_int             n,
+                                            rocblas_double_complex* x,
+                                            rocblas_int             incx, rocblas_int stridex,
+                                            rocblas_double_complex* y,
+                                            rocblas_int             incy, rocblas_int stridey, 
+                                            rocblas_int batch_count);
 
 /*! \brief BLAS Level 1 API
 
